3bd063072152f2341c0837cb0476b6d16a1da85c
cliod <qi2415@qq.com>
2020-11-05 10:42:33 +0800

    fix(0.2.x): 手机号解析
    手机号解析结果

--- ma/base.go
+++ ma/base.go
@@ -11,3 +11,28 @@ type JsCode2SessionResult struct {
 	Openid     string `json:"openid"`
 	UnionId    string `json:"unionid"`
 }
+
+type UserInfo struct {
+	Openid    string    `json:"openid"`
+	Nickname  string    `json:"nickname"`
+	Gender    string    `json:"gender"`
+	Language  string    `json:"language"`
+	City      string    `json:"city"`
+	Province  string    `json:"province"`
+	Country   string    `json:"country"`
+	AvatarUrl string    `json:"avatar_url"`
+	UnionID   string    `json:"union_id"`
+	Watermark Watermark `json:"watermark"`
+}
+
+type Watermark struct {
+	Timestamp uint64 `json:"timestamp"`
+	AppId     string `json:"appid"`
+}
+
+type PhoneNumberInfo struct {
+	PhoneNumber     string    `json:"phone_number"`
+	PurePhoneNumber string    `json:"pure_phone_number"`
+	CountryCode     string    `json:"country_code"`
+	Watermark       Watermark `json:"watermark"`
+}
--- ma/ma_user.go
+++ ma/ma_user.go
@@ -18,31 +18,6 @@ type WxMaUserService interface {
 	checkUserInfo(sessionKey, rawData, signature string) bool
 }
 
-type UserInfo struct {
-	Openid    string    `json:"openid"`
-	Nickname  string    `json:"nickname"`
-	Gender    string    `json:"gender"`
-	Language  string    `json:"language"`
-	City      string    `json:"city"`
-	Province  string    `json:"province"`
-	Country   string    `json:"country"`
-	AvatarUrl string    `json:"avatar_url"`
-	UnionID   string    `json:"union_id"`
-	Watermark Watermark `json:"watermark"`
-}
-
-type Watermark struct {
-	Timestamp string `json:"timestamp"`
-	AppId     string `json:"appid"`
-}
-
-type PhoneNumberInfo struct {
-	PhoneNumber     string    `json:"phone_number"`
-	PurePhoneNumber string    `json:"pure_phone_number"`
-	CountryCode     string    `json:"country_code"`
-	Watermark       Watermark `json:"watermark"`
-}
-
 type WxMaUserServiceImpl struct {
 	service WxMaService
 }
