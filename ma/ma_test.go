58f5760a6aef6ef8d7623ee4e9589e3a74fc9485
cliod <qi2415@qq.com>
2020-11-14 15:19:14 +0800

    test(0.6.x) 测试
    测试

---
+++ ma/ma_test.go
@@ -0,0 +1,24 @@
+package ma
+
+import (
+	"testing"
+)
+
+func TestGetAccessToken(t *testing.T) {
+	appId := "<appId>"
+	secret := "<secret>"
+
+	at, err := GetAccessToken(appId, secret)
+	if err != nil {
+		t.Log(err)
+	} else {
+		t.Logf("token是 %s ", at.AccessToken)
+	}
+
+	ws, err := CreateJsapiSignatureBy(appId, secret, "https://www.xxx.com")
+	if err != nil {
+		t.Log(err)
+	} else {
+		t.Logf("签名是 %s ", ws.Signature)
+	}
+}
